0d47bcf08593dcd1acd323d983a7d63f5f40f874
Ehfaz Mahee <mahee504@gmail.com>
2023-07-04 22:56:24 +0600

    fixed footer

--- quantam-university/src/Pages/OtherPages/About/Career/Career.jsx
+++ quantam-university/src/Pages/OtherPages/About/Career/Career.jsx
@@ -9,9 +9,7 @@ const Career = () => {
       <div className="relative">
         <img src={image1} alt="Banner" className="object-cover w-full h-64" />
         <div className="absolute inset-0 flex flex-col justify-center items-center bg-opacity-50 bg-gray-900">
-          <h1 className="text-white text-4xl font-bold">
-            General Rules and Regulations
-          </h1>
+          <h1 className="text-white text-4xl font-bold">career </h1>
         </div>
       </div>
       {/* Image Of Career  */}
--- quantam-university/src/Pages/SharedPages/Footer/Footer.jsx
+++ quantam-university/src/Pages/SharedPages/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import logo from "../../../assets/images/logo.png";
+// import logo from "../../../assets/images/logo.png";
+import logo from "../../../assets/images/bubtlogo.png";
 import { FaFacebook, FaTwitter, FaYoutube } from "react-icons/fa";
 import { Typography } from "@material-tailwind/react";
 const SITEMAP = [
@@ -26,7 +27,7 @@ const Footer = () => {
   return (
     <footer className="relative w-full bg-gray-900 text-white ">
       <div className="mx-auto w-full max-w-7xl ">
-        <div className=" ml-20 grid w-full grid-cols-1 gap-8 py-12 md:grid-cols-2 lg:grid-cols-4">
+        <div className="pl-20 grid w-full grid-cols-1 gap-8 py-12 md:grid-cols-2 lg:grid-cols-4">
           {SITEMAP.map(({ title, links }, key) => (
             <div key={key} className="w-full">
               <Typography
@@ -67,9 +68,9 @@ const Footer = () => {
           </Typography>
           <Typography
             variant="small"
-            className="mb-4 text-center font-normal text-blue-gray-900 md:mb-0"
+            className="mb-4  text-center font-normal text-blue-gray-900 md:mb-0"
           >
-            <img src={logo} alt="" />
+            <img className="h-40" src={logo} alt="" />
           </Typography>
           <div className="flex gap-4 text-blue-gray-900 sm:justify-center">
             <Typography
